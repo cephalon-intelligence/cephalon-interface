cb86e54da16b14d2b659eae340dcee695ded953b
Hart Traveller <24962209+harttraveller@users.noreply.github.com>
2023-12-19 21:02:35 -0500

    ci/utility/streamlit:

--- ci/utility/streamlit.py
+++ ci/utility/streamlit.py
@@ -1,8 +1,9 @@
 import sys
 import toml
+import streamlit as st
+import streamlit.components.v1 as components
 from pathlib import Path
 from typing import Union
-import streamlit as st
 
 
 def fix_email_prompt():
@@ -21,6 +22,28 @@ def get_url_query_parameters() -> dict[str, str]:
     for key, val in query_params.items():
         query_params[key] = val[0]
     return query_params
+
+
+def set_url_query_parameters(query_params: dict[str, str]) -> None:
+    st.experimental_set_query_params(**query_params)
+
+
+def get_node() -> Union[str, None]:
+    query_params = get_url_query_parameters()
+    if not len(query_params):
+        return None
+    elif "node" not in query_params.keys():
+        return None
+    else:
+        return query_params["node"]
+
+
+def set_node(node: str) -> None:
+    set_url_query_parameters({"node": node})
+
+
+def reset_node() -> None:
+    set_url_query_parameters(dict())
 
 
 def redirect(url: str):
@@ -42,11 +65,50 @@ def inject_css(css: str) -> None:
     st.markdown(f"<style>\n{css}\n</style>", unsafe_allow_html=True)
 
 
+def inject_hotkey_jump_to_selectbox(
+    selectbox_label: str, special: list[str] = ["shift", "meta"], character: str = "k"
+):
+    "meta (cmd/windows) + k hardcoded"
+    special_substring = [f"e.{s}Key" for s in special]
+    special_substring = " && ".join(special_substring)
+    js_code = """
+    <script>
+    const doc = window.parent.document;
+    doc.addEventListener('keydown', function(e) {"""
+    js_code += f"""
+    if ({special_substring} && e.key === '{character}') {{
+        var selectbox = doc.querySelector("input[aria-label*='{selectbox_label}']");
+        var parentDiv = selectbox.parentNode.parentNode.parentNode;
+        var clearValue = parentDiv.querySelector("svg[title='Clear value']");
+        if (clearValue) {{
+            var clickEvent = new MouseEvent("click", {{
+                bubbles: true,
+                cancelable: true,
+                view: window
+            }});
+            clearValue.dispatchEvent(clickEvent);
+        }};
+
+        if (selectbox) {{
+            e.preventDefault();
+            selectbox.click();
+            selectbox.value = '';
+        }}
+    }}"""
+
+    js_code += """
+    });
+    </script>
+    """
+    components.html(js_code, height=0, width=0)
+
+
 # todo: implement development mode
 def make_app_start_command(
     app_path: Union[str, Path],
     open_browser: bool = False,
     development_mode: bool = False,
+    magic_enabled: bool = False,
 ) -> list[str]:
     """
     subprocess start command for app
@@ -112,17 +174,17 @@ def make_app_start_command(
         "--logger.enableRich=True",
         "--client.showErrorDetails=True",
         "--client.toolbarMode=minimal",
-        "--runner.magicEnabled=False",
+        f"--runner.magicEnabled={str(magic_enabled)}",
         f"--server.headless={str(not open_browser)}",
         "--server.runOnSave=True",
         "--server.allowRunOnSave=True",
         "--server.enableCORS=True",
         "--server.enableXsrfProtection=True",
         "--browser.gatherUsageStats=False",
-        # "--theme.base=dark",
+        "--theme.base=dark",
+        "--theme.primaryColor=#4b77ff",
+        # "--theme.backgroundColor=#161616",
         "--ui.hideTopBar=True",
-        # "--theme.textColor=#0F1116",
-        # "--theme.secondaryBackgroundColor=#0F1116",
         "--server.address=localhost",
         "--server.port=31415",
     ]
